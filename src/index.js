8df3ca1d9d5fe69de9a906d34b6ebddc73833400
Camilo Beltran <cambelf.dev@gmail.com>
2021-12-23 10:52:55 -0500

    :iphone: Estilos en escritorio
    He agregado arreglos estilos correspondientes a la vista en escritorio

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-const form = document.getElementById("emailForm");
 const button = document.getElementById("formButton");
+const errorIcon =
+  '<img src="./src/assets/images/icon-error.svg" alt="error icon" class="errorIcon" />';
+const re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-button.onclick(() => {
-  const email = document.getElementById("email");
-});
+button.onclick = function () {
+  const email = document.getElementById("email").value;
 
-const validateEmail = (email) => {
-  function estaVacio(email) {
-    if (email == null || email == undefined || email == " ") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  if (estaVacio(email)) {
-    console.log("El elemento esta vacio");
+  if (email == null || email == undefined || email == " ") {
+    console.log("Correo Vacio");
+  } else {
+    // validateEmail(email);
   }
 };
+
+// const validateEmail = (email) => {
+//   const form = document.getElementById("emailForm");
+
+//   if (!re.exec(email)) {
+//     const input = document.getElementById("email");
+//     console.log(input);
+
+//     form.innerHTML += errorIcon;
+//     input.classList.add("alert");
+//   } else {
+//   }
+// };
