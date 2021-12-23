35b4374567801c4dff7e06acbb784c5e6af9f858
Camilo Beltran <cambelf.dev@gmail.com>
2021-12-23 17:52:42 -0500

    :bug: Validacion de email
    He agregado la correcion a la validacion del correo electronico

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 const button = document.getElementById("formButton");
-const errorIcon =
-  '<img src="./src/assets/images/icon-error.svg" alt="error icon" class="errorIcon" />';
 const re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-button.onclick = function () {
-  const email = document.getElementById("email").value;
+button.addEventListener("click", () => {
+  let email = document.getElementById("email").value;
+  console.log("Email: ", email);
+  const formulario = document.getElementById("emailForm");
+  const input = document.getElementById("email");
+  const message = document.querySelector(".errorMessage");
 
-  if (email == null || email == undefined || email == " ") {
-    console.log("Correo Vacio");
-  } else {
-    // validateEmail(email);
-  }
-};
-
-// const validateEmail = (email) => {
-//   const form = document.getElementById("emailForm");
-
-//   if (!re.exec(email)) {
-//     const input = document.getElementById("email");
-//     console.log(input);
-
-//     form.innerHTML += errorIcon;
-//     input.classList.add("alert");
-//   } else {
-//   }
-// };
+  formulario.addEventListener("submit", (e) => {
+    const img = document.createElement("img");
+    if (!re.test(email)) {
+      message.innerHTML = "Please provide a valid email";
+      input.style.borderColor = "hsl(0, 93%, 68%)";
+      img.setAttribute("src", "./src/assets/images/icon-error.svg");
+      img.setAttribute("class", "errorIcon");
+      formulario.appendChild(img);
+      formulario.appendChild(message);
+      button.style.background = "hsl(0, 80%, 86%)";
+      button.style.boxShadow = "3px 3px 18px 0px hsl(0, 6%, 24%)";
+    } else {
+      if (document.querySelector(".errorIcon")) {
+        input.style.borderColor = "hsl(0, 36%, 70%)";
+        message.innerHTML = "Success";
+        let imagenes = document.querySelectorAll(".errorIcon");
+        imagenes.forEach((imagen) => {
+          imagen.remove();
+        });
+      }
+    }
+    e.preventDefault();
+  });
+});
